04ecb56f32f528fdbc7b022da1f3de1ac885358c
W.C. Pannell <2120605+wcpannell@users.noreply.github.com>
2023-04-11 15:49:27 -0500

    Allow VIM-Plug to install before trying to use it
    Simplified deploy script now requires that you be in the project root directory to run it.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-# move to repo directory
-cd "${0%/*}"
+# Must be run from this directory
 
 #install dotfiles
-mv dotfiles/.vimrc ~/.vimrc
+mv .vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
-mv dotfiles/init.vim ~/.config/nvim/init.vim
+mv init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.config/coc
-mv dotfiles/coc-settings.json ~/.config/coc
+mv coc-settings.json ~/.config/coc
 
-# Set up nvim
+# Allow VIM-Plug to autoinstall
+nvim +qall
+# Install Plugins
 nvim +'PlugInstall' +qall
+# Potentially pointless update of CoC
 nvim +'CocUpdateSync' +qall
 
-# install coc extensions
+# Install CoC extensions
 nvim +'CocInstall coc-pyright coc-clangd coc-rust-analyzer coc-json -sync' +qall
