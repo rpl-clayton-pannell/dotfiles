2de71ddde9fd50290e51dc2f44d0d478f4ee5812
Clayton Pannell <cpannell@cdielectronics.com>
2023-12-15 09:14:13 -0600

    Use LSP instead of CoC. Only tested with neovim

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,9 @@
 #install dotfiles
 mv .vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
-mv init.vim ~/.config/nvim/init.vim
-mkdir -p ~/.config/coc
-mv coc-settings.json ~/.config/coc
+mv init.lua ~/.config/nvim/init.lua
 
 # Allow VIM-Plug to autoinstall
 nvim +qall
 # Install Plugins
 nvim +'PlugInstall' +qall
-# Potentially pointless update of CoC
-nvim +'CocUpdateSync' +qall
-
-# Install CoC extensions
-nvim +'CocInstall coc-pyright coc-clangd coc-rust-analyzer coc-json -sync' +qall
--- init.vim
+++
@@ -1,4 +0,0 @@
-let g:loaded_python_provider = 0
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath = &runtimepath
-source ~/.vimrc
